00423b73e299ef44bbb30cf62f65295452607858
Simon C Blyth <blyth@hep1.phys.ntu.edu.tw>
2011-04-20 21:05:54 +0800

    working tabledoc albeit messily ... needs sectioning and titles

--- converter/sphinxext/tabledoc.py
+++ converter/sphinxext/tabledoc.py
@@ -5,6 +5,7 @@
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     Based on the simplest sphinx directive: :py:mod:`sphinx.ext.ifconfig`
+    and http://sphinx.pocoo.org/ext/tutorial.html#the-directive-classes
 
     Usage::
 
@@ -18,15 +19,36 @@
 import os
 from docutils import nodes
 from sphinx.util.compat import Directive
+from docutils.statemachine import ViewList
+
 from converter.tabular import TabularData
 from ConfigParser import SafeConfigParser as ConfigParser
+
 import MySQLdb 
 import MySQLdb.cursors
 
-class tabledoc(nodes.Element): pass
+ 
+class MySQLDocumenter(object):
+    def __init__(self, directive, arguments , indent=u''):
+        self.directive = directive
+        self.arguments = arguments
+        self.indent = indent
+    def add_line(self, line, source, *lineno):
+        """Append one line of generated reST to the output."""
+        self.directive.result.append(self.indent + line, source, *lineno)
+
+    def generate(self):
+        sect, table = self.arguments                
+        db = MyDB(sect)
+        desc = db.table_description(table)
+        for line in str(desc).split("\n"): 
+            self.add_line( self.indent + line , "mysql tabledoc for %s" % table , 0 )
+
 
 class TableDoc(Directive):
-
+    """
+    Follow autodoc pattern
+    """
     has_content = False
     required_arguments = 2
     optional_arguments = 0
@@ -34,30 +56,27 @@ class TableDoc(Directive):
     option_spec = {}
 
     def run(self):
-        node = tabledoc()
+        # generate the output
+        self.result = ViewList()  
+        documenter = MySQLDocumenter(self, self.arguments )
+        documenter.generate()
+
+        node = nodes.paragraph()
         node.document = self.state.document
-        node.line = self.lineno
-        node['db_sect'] = self.arguments[0]
-        node['db_table'] = self.arguments[1]
-        self.state.nested_parse(self.content, self.content_offset,
-                                node, match_titles=1)
-        return [node]
+        self.state.nested_parse(self.result, 0, node)
+        return node.children
 
 
 class MyCnf(dict):
-    """
-    annoyingly needed due to mysql vs mysql-python configfile key incompatibility preventing
-        con = MySQLdb.connect( read_default_group=node['db_sect'] )
-    """ 
     def __init__(self, sect):
         cfp = ConfigParser()
         cfp.read(os.path.expanduser("~/.my.cnf"))
         self.update( dict( cfp.items(sect) ))
 
 class MyDB(object):
-    def __init__(self, cnf ):
-        if type(cnf) == str:
-            cnf = MyCnf(cnf)
+    def __init__(self, sect ):
+        cnf = MyCnf(str(sect))
+        print cnf
         con = MySQLdb.connect( host=cnf['host'], db=cnf['database'], user=cnf['user'], passwd=cnf['password'], cursorclass=MySQLdb.cursors.DictCursor )
         cur = con.cursor() 
         self.con = con
@@ -69,39 +88,16 @@ class MyDB(object):
          return map(lambda _:_.values()[0], self("show tables"))
     tables = property( _tables )
 
-
-
-def describe( sect , table ):
-    db = MyDB( sect )
-    return [d for d in db("describe %s" % table)] 
-
-def process_tabledoc_nodes(app, doctree, docname):
-
-    for node in doctree.traverse(tabledoc):
-        try:
-            desc = describe( node['db_sect'], node['db_table'] )             
-        except Exception, err:
-            # handle exceptions in a clean fashion
-            from traceback import format_exception_only
-            msg = ''.join(format_exception_only(err.__class__, err))
-            newnode = doctree.reporter.error('Exception occured in '
-                                             'tabledoc: \n%s' %
-                                             msg, base_node=node)
-            node.replace_self(newnode)
-        else:
-            node.replace_self(node.children)
+    def table_description(self, t ):
+        return TabularData( self("describe %s" % t ))
 
 
 def setup(app):
-    app.add_node(tabledoc)
     app.add_directive('tabledoc', TableDoc)
-    app.connect('doctree-resolved', process_tabledoc_nodes)
 
 
 if __name__=='__main__':
-
     db = MyDB('dcs')
     for t in db.tables:
-        td = TabularData( db("describe %s" % t ))
-        print td 
+        print db.table_description(t) 
 
